20c970fd51c969b24ec0aade85bae099cc6db32b
ptjb <ptjb@users.noreply.github.com>
2017-05-09 13:50:34 +0100

    Test_Git
    Testing Commitment Functionality

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -2,16 +2,38 @@
 
 namespace GildedRose.Console
 {
-    class Program
+    public class Program                    //will return to internal once I finish tests
     {
-        IList<Item> Items;
-        static void Main(string[] args)
+        public IList<Item> Items;           //will return to private once I finish tests
+
+
+        //If Main gets too annoying
+        /*public Program()
+        {
+            Items = new List<Item>
+                                          {
+                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                                              new Item
+                                                  {
+                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
+                                                      SellIn = 15,
+                                                      Quality = 20
+                                                  },
+                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                                              };
+        }*/
+
+        public static void Main(string[] args)
+        //public Program()
         {
             System.Console.WriteLine("OMGHAI!");
 
             var app = new Program()
-                          {
-                              Items = new List<Item>
+                                    {
+                                        Items = new List<Item>
                                           {
                                               new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                                               new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
@@ -25,8 +47,7 @@ namespace GildedRose.Console
                                                   },
                                               new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
                                           }
-
-                          };
+                                    };
 
             app.UpdateQuality();
 
--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,4 +1,7 @@
 using Xunit;
+using GildedRose.Console;
+using System.Collections.Generic;
+//using System;
 
 namespace GildedRose.Tests
 {
@@ -9,5 +12,52 @@ namespace GildedRose.Tests
         {
             Assert.True(true);
         }
+        /*
+        [Fact]
+        public void TestTheEqual()
+        {
+            Assert.Equal(4, (2+2));
+        }
+
+        [Fact]
+        public void TestTheFalse()
+        {
+            Assert.False(false);
+        }*/
+
+        //static public void Main(string[] args)
+
+        [Fact]
+        public void TestInitialReduction()
+        {
+            Program test = new Program()
+            {
+                Items = new List<Item>
+                    {
+                        new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                        new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                        new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                        new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                        new Item
+                            {
+                                Name = "Backstage passes to a TAFKAL80ETC concert",
+                                SellIn = 15,
+                                Quality = 20
+                            },
+                        new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                    }
+            };
+
+            //string[] a = new string[0];
+            //Program.Main(a);
+            //Program.Main(a);
+            //test.UpdateQuality();
+            //System.Console.WriteLine("Item 0: {0}\t{1}\t{2}", test.Items[0].Name, test.Items[0].SellIn, test.Items[0].Quality);
+            Assert.True(test.Items.Count != 0);
+            //Assert.Equal(9, test.Items[0].SellIn);
+            //System.Console.ReadKey();
+        }
+
+
     }
 }
