07b3deb02237daea385ea7419fdc9aa148dcbcc4
ptjb <ptjb@users.noreply.github.com>
2017-05-10 21:29:37 +0100

    Refactoring v1.1
    Put SellIn adjustment into its own method. Written a method for keeping
    Quality within bounds, will implement with some restructuring next time.

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -38,77 +38,124 @@ namespace GildedRose.Console
         {
             foreach (var Good in Stock)
             {
-                if (Good.Name != "Aged Brie" && Good.Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Good.Quality > 0)
-                    {
-                        if (Good.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Good.Quality -= 1;		
-                        }
-                    }
-                }
-                else
-                {
-                    if (Good.Quality < 50)
-                    {
-                        Good.Quality += 1;
+				/*
+				//Adjust Quality value based on the Good
+				AdjustQualityByGood(Good);
 
-                        if (Good.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Good.SellIn < 11)
-                            {
-                                if (Good.Quality < 50)
-                                {
-                                    Good.Quality += 1;
-                                }
-                            }
+				//Adjust SellIn value based on the Good
+				AdjustSellInByGood(Good);
+				*/
 
-                            if (Good.SellIn < 6)
-                            {
-                                if (Good.Quality < 50)
-                                {
-                                    Good.Quality += 1;
-                                }
-                            }
-                        }
-                    }
-                }
-				
-                if (Good.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Good.SellIn = Good.SellIn - 1;
-                }
+				//Standard Quality Adjustment
+				if (Good.Name != "Aged Brie" && Good.Name != "Backstage passes to a TAFKAL80ETC concert")
+				{
+					if (Good.Quality > 0)
+					{
+						if (Good.Name != "Sulfuras, Hand of Ragnaros")
+						{
+							Good.Quality -= 1;
+						}
+					}
+				}
+				else
+				{
+					if (Good.Quality < 50)
+					{
+						Good.Quality += 1;
 
-                if (Good.SellIn < 0)
-                {
-                    if (Good.Name != "Aged Brie")
-                    {
-                        if (Good.Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Good.Quality > 0)
-                            {
-                                if (Good.Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Good.Quality -= 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Good.Quality = 0;
-                        }
-                    }
-                    else
-                    {
-                        if (Good.Quality < 50)
-                        {
-                            Good.Quality += 1;
-                        }
-                    }
-                }
-            }
+						if (Good.Name == "Backstage passes to a TAFKAL80ETC concert")
+						{
+							if (Good.SellIn < 11)
+							{
+								if (Good.Quality < 50)
+								{
+									Good.Quality += 1;
+								}
+							}
+
+							if (Good.SellIn < 6)
+							{
+								if (Good.Quality < 50)
+								{
+									Good.Quality += 1;
+								}
+							}
+						}
+					}
+				}
+
+				//SellIn adjustment
+				Good.SellIn = AdjustSellInByGood(Good.Name, Good.SellIn);
+
+				//Adjustment if Good IS/HAS-JUST-GONE Out-of-Date
+				if (Good.SellIn < 0)
+				{
+					if (Good.Name != "Aged Brie")
+					{
+						if (Good.Name != "Backstage passes to a TAFKAL80ETC concert")
+						{
+							if (Good.Quality > 0)
+							{
+								if (Good.Name != "Sulfuras, Hand of Ragnaros")
+								{
+									Good.Quality -= 1;
+								}
+							}
+						}
+						else
+						{
+							Good.Quality = 0;
+						}
+					}
+					else
+					{
+						if (Good.Quality < 50)
+						{
+							Good.Quality += 1;
+						}
+					}
+				}
+			}
         }
+
+
+		//Decides how to change the Good Quality based on the requested change
+		//I would ideally try to put this in the Item class but I'm not allowed
+		private int BoundedQualityAdjust(int Quality, int Change)
+		{
+			Quality += Change;
+			
+			//Keeps Quality within limits
+			if (Quality > 50) return 50;
+			if (Quality < 0) return 0;
+				
+			return Quality;
+		}
+
+		private int AdjustSellInByGood(string Name, int SellIn)
+		{
+			if (Name == "Sulfuras, Hand of Ragnaros") return SellIn;
+			return (SellIn - 1);
+		}
+		/*
+		private void AdjustQualityByGood(Item Good)
+		{
+			
+			//Alters the Quality Decay amount if Good is out-of-date
+			int OffMultiplier = 1;
+			
+
+		}
+		*/
+
+		/*
+		//Move additional Quality alterations to the other method
+		private void AdjustSellInByGood(Item Good)
+		{
+
+			
+		}
+		*/
 
     }
 
